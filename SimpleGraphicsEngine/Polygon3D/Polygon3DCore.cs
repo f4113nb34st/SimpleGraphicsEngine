1856022fb9636a896857fe4a187d186a0ba49075
f4113nb34st <thefatgamer99@yahoo.com>
2015-10-12 22:04:38 -0500

    Added core class

---
+++ SimpleGraphicsEngine/Polygon3D/Polygon3DCore.cs
@@ -0,0 +1,41 @@
+﻿namespace SimpleGraphicsEngine.Polygon3D
+{
+	using System;
+	using IROM.Core;
+	using IROM.Util;
+
+	public class Polygon3DCore : Core
+	{
+		public static void Main()
+		{
+			try
+			{
+				new Polygon3DCore().Start();
+			}catch (Exception ex)
+ 			{
+				Console.WriteLine(ex);
+			}
+		}
+
+		public Polygon3DCore() : base("Polygon3DCore")
+		{
+			
+		}
+
+		protected override void Init()
+		{
+			//close on window X
+			WindowObj.OnClose += ((sender, args) => Running = false);
+		}
+
+		protected override void Tick()
+		{
+			
+		}
+
+		protected override void Render(Image image)
+		{
+			
+		}
+	}
+}
